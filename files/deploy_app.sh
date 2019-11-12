1cdb908e669cd908925bb3ef9995c5084447d0e8
Mat White <github@matbert.com>
2019-11-12 21:17:41 +0000

    Tablify.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,10 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
+  <table><tr><td>
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><img src="https://i.pinimg.com/474x/a5/5d/6c/a55d6c648a0c90098018e0fdf7f8dce7--carnifex-deathcore.jpg"></img></center>
-  <center><h2>\m/ Meow World! \m/</h2></center>
+  </td><td><center><img src="https://i.pinimg.com/474x/a5/5d/6c/a55d6c648a0c90098018e0fdf7f8dce7--carnifex-deathcore.jpg"></img></center>
+  <center></td></tr></table><h2>\m/ Meow World! \m/</h2></center>
   Welcome to ${PREFIX}'s app. Rock on!!!
   <!-- END -->
   
